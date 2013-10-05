be258f136b4a6537a9325727ca221c5381642a13
Jörg Richter <jri@deepamehta.de>
2013-10-05 15:50:40 +0200

    Adapt to optimized click detection.

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -30,9 +30,10 @@ dm4c.add_plugin("de.deepamehta.boxrenderer", function() {
         // ---
 
         /**
-         * Adds "width" and "height" properties to the topic view. The CanvasView relies on these for click detection.
+         * Adds "x1", "y1", "x2", "y2" properties to the topic view. Click detection relies on this bounding box.
+         * Adds "width" and "height" proprietary properties.         Updated on topic update (label or type changed).
          * Adds "label_wrapper", "icon_size" proprietary properties. Updated on topic update (label or type changed).
-         * Adds "box_pos", label_pos", "icon_pos" proprietary properties. Updated on topic move.
+         * Adds "label_pos", "icon_pos" proprietary properties.      Updated on topic move.
          *
          * @param   tv      A TopicView object (defined in CanvasView),
          *                  has "id", "type_uri", "label", "x", "y" properties.
@@ -49,7 +50,7 @@ dm4c.add_plugin("de.deepamehta.boxrenderer", function() {
         this.draw_topic = function(tv, ctx) {
             // 1) box
             ctx.fillStyle = BOX_COLOR
-            ctx.fillRect(tv.box_pos.x, tv.box_pos.y, tv.width, tv.height)
+            ctx.fillRect(tv.x1, tv.y1, tv.width, tv.height)
             // 2) label
             ctx.fillStyle = LABEL_COLOR
             tv.label_wrapper.draw(tv.label_pos.x, tv.label_pos.y, ctx)
@@ -62,7 +63,7 @@ dm4c.add_plugin("de.deepamehta.boxrenderer", function() {
         }
 
         this.on_mousedown = function(pos, modifier) {
-            var tv = find_topic_via_icon(pos.topicmap)
+            var tv = detect_topic_via_icon(pos.topicmap)
             if (tv) {
                 dm4c.do_select_topic(tv.id)
                 dm4c.topicmap_renderer.begin_association(tv.id, pos.canvas.x, pos.canvas.y)
@@ -77,37 +78,37 @@ dm4c.add_plugin("de.deepamehta.boxrenderer", function() {
             // label
             tv.label_wrapper = new js.TextWrapper(tv.label, dm4c.MAX_TOPIC_LABEL_WIDTH, LABEL_LINE_HEIGHT, ctx)
             var size = tv.label_wrapper.get_size()
-            tv.width = Math.max(size.width + BOX_PAD_HORIZ, LABEL_LINE_HEIGHT)
+            tv.width = Math.max(size.width + BOX_PAD_HORIZ,  LABEL_LINE_HEIGHT)
             tv.height = Math.max(size.height + BOX_PAD_VERT, LABEL_LINE_HEIGHT)
             // icon
             var icon = dm4c.get_type_icon(tv.type_uri)
             tv.icon_size = {
-                width: icon.width / ICON_SCALE_FACTOR,
+                width: icon.width   / ICON_SCALE_FACTOR,
                 height: icon.height / ICON_SCALE_FACTOR
             }
         }
 
         function update_geometry(tv) {
-            // box
-            tv.box_pos = {
-                x: tv.x - tv.width / 2,
-                y: tv.y - tv.height / 2
-            }
+            // bounding box
+            tv.x1 = tv.x - tv.width  / 2,
+            tv.y1 = tv.y - tv.height / 2
+            tv.x2 = tv.x1 + tv.width
+            tv.y2 = tv.y1 + tv.height
             // label
             tv.label_pos = {
-                x: tv.box_pos.x + LABEL_OFFSET_X,
-                y: tv.box_pos.y + LABEL_LINE_HEIGHT + LABEL_OFFSET_Y
+                x: tv.x1 + LABEL_OFFSET_X,
+                y: tv.y1 + LABEL_LINE_HEIGHT + LABEL_OFFSET_Y
             }
             // icon
             tv.icon_pos = {
-                x: tv.box_pos.x + tv.width - tv.icon_size.width / ICON_OFFSET_FACTOR,
-                y: tv.box_pos.y + tv.height - tv.icon_size.height / ICON_OFFSET_FACTOR
+                x: tv.x2 - tv.icon_size.width  / ICON_OFFSET_FACTOR,
+                y: tv.y2 - tv.icon_size.height / ICON_OFFSET_FACTOR
             }
         }
 
         // ---
 
-        function find_topic_via_icon(pos) {
+        function detect_topic_via_icon(pos) {
             return canvas_view.iterate_topics(function(tv) {
                 if (pos.x >= tv.icon_pos.x && pos.x < tv.icon_pos.x + tv.icon_size.width &&
                     pos.y >= tv.icon_pos.y && pos.y < tv.icon_pos.y + tv.icon_size.height) {
